05d00e1009afcf0d81914f6972b4820dfb1c00a9
Edgard Patete <pateteedgard@gmail.com>
2026-05-03 11:04:01 -0400

    avance del formulario de edición de paginas

--- frontend/Html/admin/editor-pagina.php
+++ frontend/Html/admin/editor-pagina.php
@@ -1,5 +1,32 @@
+<?php 
+extract($config);
+
+function listText(array $array):string{
+    $list = '';
+    $nItems = count($array);
+    
+    foreach($array as $id => $item){
+      $list .=  $item;
+      if($id +1 < $nItems ) $list .= ',';
+    }
+    return $list;
+}
+?>
 <form >
-  <input type="text" name="" value="">
-  
-  
+<h2>titulo de pagina</h2>
+  <input type="text" name="tituloPagina" value="<?= $tituloPagina ?? '' ?>">
+ 
+
+ <input type="text" name="estilos" value="<?= listText($estilosD ?? []) ?>">
+  <input type="text" name="estilos" value="<?= listText($estilos ?? []) ?>">
+<input type="text" name="estilos" value="<?= listText($scriptsD ?? []) ?>">
+<input type="text" name="estilos" value="<?= listText($scripts ?? []) ?>">
+
+  <?php foreach($contenidos as $contenido): ?>
+
+
+<?php endforeach; ?>
+
+ <?php print_r($config ?? [] ); ?>
 </form>
+
